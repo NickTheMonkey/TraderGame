dced1c22941234db071552c564d589a71d865d59
NickTheMonkey <6wolf92@gmail.com>
2021-11-29 22:33:55 +0200

    -изменил ф-ию возврата класса тайла по ID (не работает)

--- Source/TraderGame/Private/CPP/DATA/DataAssets/DA_TilesMeshKeys.cpp
+++ Source/TraderGame/Private/CPP/DATA/DataAssets/DA_TilesMeshKeys.cpp
@@ -3,16 +3,6 @@
 
 #include "CPP/DATA/DataAssets/DA_TilesMeshKeys.h"
 
-bool UDA_TilesMeshKeys::GetTileType_OLD_Implementation(const int32& tileID, TSubclassOf<ACPP_Tile_Base>& tileType)
-{
-	if(NumberToTile.Contains(tileID))
-	{
-		tileType = *NumberToTile.Find(tileID);
-		return true;
-	}
-	return false;
-}
-
 void UDA_TilesMeshKeys::GetKeyRange_Implementation(int32& firstKey, int32& lastKey)
 {
 	firstKey = FirstKeyNumber;
@@ -24,10 +14,14 @@ float UDA_TilesMeshKeys::GetTileSize_Implementation()
 	return TileSize;
 }
 
-TSubclassOf<ACPP_Tile_Base>& UDA_TilesMeshKeys::GetTileType_Implementation(const int32& tileID)
+TSubclassOf<ACPP_Tile_Base> UDA_TilesMeshKeys::GetTileType_Implementation(const int32& tileID)
 {
 	if(NumberToTile.Contains(tileID))
 	{
-		return *NumberToTile.Find(tileID);
+		//TODO: функция неправильно возвращает класс
+		TSubclassOf<ACPP_Tile_Base>* result = NumberToTile.Find(tileID);
+		return *result;
 	}
+	TSubclassOf<ACPP_Tile_Base> nores;
+	return nores;
 }
